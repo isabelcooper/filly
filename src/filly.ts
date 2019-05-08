6616cc8b6aeb625c1ffdd4304e49d001255ee462
Tom Damant <tomdamant@hotmail.com>
2019-05-08 15:51:53 +0100

    test all params handed through

--- src/filly.ts
+++ src/filly.ts
@@ -1,10 +1,10 @@
+import {Page} from "puppeteer";
 
-export const takeScreenshot = async (page) => {
+export const takeScreenshot = async (page: Page) : Promise<void> => {
     await page.goto('https://airtable.com/shri36qqm4SB9dTTC');
     await page.waitForSelector(".formName");
     await page.type(".col-12.line-height-4","hello");
     await page.type(".date.truncate.px1","9/5/2019");
-    await page.screenshot({path: "date.png"})
     await page.evaluate(() => {
         const option = document.querySelectorAll(".sharedFormField")[2].querySelectorAll(".items-center.flex-none.circle")[2] as HTMLSelectElement;
         option.click()
@@ -12,4 +12,5 @@ export const takeScreenshot = async (page) => {
     await page.click(".submitButton");
     await page.screenshot({path: "third.png"});
     await page.close();
+    return
 };
--- test/filly.int.test.ts
+++ test/filly.int.test.ts
@@ -1,21 +1,28 @@
 import * as puppeteer from "puppeteer";
 import { takeScreenshot } from "../src/filly";
 
+function parseFormParams(requestBody: string) {
+  const relevantString = requestBody.split(`name="file"`)[1].split("--")[0];
+  return JSON.parse(relevantString).cellValuesByColumnId;
+}
+
 describe('Filly', () => {
     it('sends request to airtable', async() => {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.setRequestInterception(true);
-        let requestBody = ""
+        let requestBody = "";
         page.on('request', (interceptedReq) =>{
-            console.log(interceptedReq.url());
-            if (interceptedReq.url() === "https://airtable.com/v0.3/view/viwMGduVHfcJ0QcUp/submitSharedForm") {
-                requestBody = interceptedReq.postData()
+            if (interceptedReq.url() === "https://airtable-inbound-production.s3.amazonaws.com/") {
+              requestBody = interceptedReq.postData()
             }
             interceptedReq.continue()
         });
         await takeScreenshot(page);
         await browser.close();
-        expect(requestBody).toContain("hello")
+        const parsedParams = parseFormParams(requestBody);
+        expect(parsedParams).toEqual({ fldyWzC1p5jzDzWPs: 'hello',
+        fldkdY4SoeqSLjMJ7: '2019-09-05T00:00:00.000Z',
+        fldM0AU0NqHM2UgcS: 'seli3qiIewebEMoik' })
     });
 });
