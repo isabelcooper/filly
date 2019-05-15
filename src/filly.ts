33774c9a9edc126f314a30e29914fa2fa1c4631e
isabelcooper <isabel@triptease.com>
2019-05-15 15:41:34 +0100

    test to select option (using event listener)

--- src/filly.ts
+++ src/filly.ts
@@ -1,4 +1,5 @@
 import {Page} from "puppeteer";
+import {format} from "@bodar/totallylazy/dates";
 
 
 interface FormFiller {
@@ -21,18 +22,15 @@ export class MakersFormFiller implements FormFiller {
     // await this.page.goto(url);
     // await this.page.waitForSelector(".formName");
 
-    await this.page.type(".date.truncate.px1","9/5/2019");
-
     await this.selectOptions();
     await this.page.click(".submitButton");
 
     return
   }
 
-  private async selectOptions() {
+  public async selectOptions() {
     await this.page.evaluate((async () => {
       const inputElements = document.querySelectorAll(".cellContainer>.formCell");
-
       const optionsArray = Array.from(inputElements).filter((selectElement) => {
           const potentialSelect = selectElement as HTMLSelectElement;
           return potentialSelect.dataset.columntype === "select"
@@ -50,4 +48,10 @@ export class MakersFormFiller implements FormFiller {
   public async fillName(name: string) {
     await this.page.type(".col-12.line-height-4", name);
   }
+
+  public async fillDate() {
+    const dateToday = format(new Date(), "en", "dd/MM/yyyy");
+    await this.page.type(".date.truncate.px1", dateToday);
+  }
+
 }
--- test/filly.int.test.ts
+++ test/filly.int.test.ts
@@ -1,6 +1,7 @@
 import {evalFunction, MakersFormFiller, NewPageType} from "../src/filly";
 import {JSDOM} from 'jsdom';
 import {airtableHTML} from "./fakeHTML/airtableHTML";
+import {format} from "@bodar/totallylazy/dates";
 
 
 export class JsdomTranslator implements NewPageType {
@@ -14,7 +15,7 @@ export class JsdomTranslator implements NewPageType {
   }
 
   evaluate(evaluateFunction: evalFunction): void {
-    this.pageObject.window.eval(`${evaluateFunction}`)
+    this.pageObject.window.eval(`(${evaluateFunction})()`)
   }
 
   type(selector: string, value: string): void {
@@ -26,15 +27,44 @@ export class JsdomTranslator implements NewPageType {
     return this.pageObject.window.document.querySelector(selector)
   }
 
+  querySelectorAll(selector: string): NodeList {
+    return this.pageObject.window.document.querySelectorAll(selector)
+  }
+
+  addEventListener(type: string, element: HTMLElement, callback: () => any ) {
+    element.addEventListener(type, callback)
+  }
 }
 
 
 describe('Filly', () => {
+
     it('it can fill in the name of the user', async() => {
       const page = new JsdomTranslator(new JSDOM(airtableHTML));
       const testFormFiller = new MakersFormFiller(page);
       await testFormFiller.fillName("Tom");
       // @ts-ignore
       expect(page.querySelector(".col-12.line-height-4").value).toEqual("Tom")
+    });
+
+    it('it can fill date today by default', async() => {
+      const page = new JsdomTranslator(new JSDOM(airtableHTML));
+      const testFormFiller = new MakersFormFiller(page);
+      await testFormFiller.fillDate();
+      // @ts-ignore
+      expect(page.querySelector(".date.truncate.px1").value).toEqual(format(new Date(),"en", "dd/MM/yyyy"))
+    });
+
+    it('it selects green on a picklist', async() => {
+      const page = new JsdomTranslator(new JSDOM(airtableHTML, { runScripts: "outside-only" }));
+      const testFormFiller = new MakersFormFiller(page);
+      // @ts-ignore
+      const targetCircles = page.querySelectorAll(".items-center.flex-none.circle");
+      const clicks: number[] = [];
+
+      targetCircles.forEach((circle, index) => page.addEventListener("click", circle as HTMLElement, () =>{clicks.push(index)}));
+      await testFormFiller.selectOptions();
+
+      expect(clicks).toEqual([2]);
     });
 });
