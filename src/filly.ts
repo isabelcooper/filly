cb7ed8eb9aecb1529dcf8047ae7cb9e772f5cb32
isabelcooper <isabel@triptease.com>
2019-05-15 10:46:26 +0100

    selectoptions without using string as arg

--- src/filly.ts
+++ src/filly.ts
@@ -16,28 +16,27 @@ export class MakersFormFiller implements FormFiller {
     await this.page.type(".col-12.line-height-4", name);
     await this.page.type(".date.truncate.px1","9/5/2019");
 
-    await this.selectOptions(2);
+    await this.selectOptions();
     await this.page.click(".submitButton");
 
     return
   }
 
-  private async selectOptions(choice: number) {
-    await this.page.evaluate(`(async () => {
+  private async selectOptions() {
+    await this.page.evaluate((async () => {
       const inputElements = document.querySelectorAll(".cellContainer>.formCell");
-      console.log(inputElements);
+
       const optionsArray = Array.from(inputElements).filter((selectElement) => {
-          const potentialSelect = selectElement;
+          const potentialSelect = selectElement as HTMLSelectElement;
           return potentialSelect.dataset.columntype === "select"
         });
-      await optionsArray.forEach(async option => {
-      const circle = option.querySelectorAll(".items-center.flex-none.circle")[${choice}];
-      circle.click();
-      await page.screenshot({path: "test.png"});
-      return
+
+      await optionsArray.forEach( (option) => {
+        const circle = option.querySelectorAll(".items-center.flex-none.circle")[2] as HTMLSelectElement;
+        circle.click();
+        return
       });
-    })()`);
 
-
+    }));
   }
 }
